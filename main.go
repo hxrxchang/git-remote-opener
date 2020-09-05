72c874f1f64f2548494eba9bfaf8c46f59395468
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-09-06 02:30:21 +0900

    exit code

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"regexp"
 	"strings"
@@ -13,11 +14,13 @@ func main() {
 	out, err := exec.Command("git", "remote", "-v").Output()
 	if err != nil {
 		fmt.Println("fatal: not a git repository (or any of the parent directories): .git")
+		os.Exit(1)
 		return
 	}
 	stringified := string(out)
 	if stringified == "" {
 		fmt.Println("fatal: 'origin' does not appear to be a git repository\nfatal: Could not read from remote repository.\n\nPlease make sure you have the correct access rights\nand the repository exists.")
+		os.Exit(1)
 		return
 	}
 	replaced := strings.Replace(stringified, `\n`, "\n", -1)
