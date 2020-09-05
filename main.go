6621ebfd5bd2055bb9849e800f8a2276d4b07227
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-09-05 15:53:58 +0900

    remoteリポジトリURLを取得

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello world")
+	out, _ := exec.Command("git", "remote", "-v").Output()
+	stringified := string(out)
+	replaced := strings.Replace(stringified, `\n`, "\n", -1)
+	splited := strings.Split(replaced, "\n")
+	origin := splited[0]
+	origin = "https://github.com/" + getOrigin(origin)
+	fmt.Println(origin)
+}
+
+func getOrigin(str string) string {
+	reg := regexp.MustCompile(`:(.*).git`)
+	s := reg.FindStringSubmatch(str)
+	return s[1]
 }
