ad6eccda7af900dd6481bf9714ffb823158af99a
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-09-22 21:46:09 +0900

    fix to support other services in addition to GitHub

--- git_repo/git_repo.go
+++ git_repo/git_repo.go
@@ -2,10 +2,24 @@ package git_repo
 
 import "regexp"
 
-func GetRepoUrl(str string) string {
-	// put "?:" not to capture
-	reg := regexp.MustCompile(`(?:git@github.com:|https:\/\/github.com\/)(.*).git`)
-	s := reg.FindStringSubmatch(str)
-	// get value of s[1] to get captured value by "(.*)"
-	return "https://github.com/" + s[1]
+type RepoInfo struct {
+	Hostname, User, Repo string
+}
+
+func GetRepoUrl(remoteUrlString string) string {
+	whenSshRegexp := regexp.MustCompile(`^origin\s+git@`)
+	whenHttpsRegexp := regexp.MustCompile(`^origin\s+https:`)
+	var repoInfo RepoInfo
+	if whenSshRegexp.MatchString(remoteUrlString) {
+		repoInfo = buildRepoInfo(remoteUrlString, `^origin\s+git@(.*):(.*)\/(.*).git`)
+	} else if whenHttpsRegexp.MatchString(remoteUrlString) {
+		repoInfo = buildRepoInfo(remoteUrlString, `^origin\s+https:\/\/(.*)\/(.*)\/(.*).git`)
+	}
+	return "https://" + repoInfo.Hostname + "/" + repoInfo.User + "/" + repoInfo.Repo
+}
+
+func buildRepoInfo(remoteUrlString string, regexpString string) RepoInfo {
+	regexp := regexp.MustCompile(regexpString)
+	matchingResult := regexp.FindStringSubmatch(remoteUrlString)
+	return RepoInfo{matchingResult[1], matchingResult[2], matchingResult[3]}
 }
