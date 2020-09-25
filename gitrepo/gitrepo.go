7de0b70d9e591e7cf17cd4b7c15969086bff37e2
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-09-25 22:10:26 +0900

    fix error message

--- gitrepo/gitrepo.go
+++ gitrepo/gitrepo.go
@@ -9,6 +9,8 @@ type repoInfo struct {
 	Hostname, User, Repo string
 }
 
+const invalidURLMessage string = "invalid git remote url.\nPlease run 'git remote -v' to check it."
+
 // GetRepoURL is a function to get string which means git remote repository's url and is started with https://.+
 func GetRepoURL(remoteURLString string) (string, error) {
 	whenSSHRegexp := regexp.MustCompile(`^origin\s+git@`)
@@ -20,10 +22,10 @@ func GetRepoURL(remoteURLString string) (string, error) {
 	} else if whenHTTPSRegexp.MatchString(remoteURLString) {
 		repoInfoVal, repoInfoErr = buildRepoInfo(remoteURLString, `^origin\s+https:\/\/(.*)\/(.*)\/(.*).git`)
 	} else {
-		return "", errors.New("something went wrong")
+		return "", errors.New(invalidURLMessage)
 	}
 	if repoInfoErr != nil {
-		return "", errors.New("something went wrong")
+		return "", errors.New(invalidURLMessage)
 	}
 	return "https://" + repoInfoVal.Hostname + "/" + repoInfoVal.User + "/" + repoInfoVal.Repo, nil
 }
@@ -32,7 +34,7 @@ func buildRepoInfo(remoteURLString string, regexpString string) (repoInfo, error
 	repoInfoRegexp := regexp.MustCompile(regexpString)
 	matchingResult := repoInfoRegexp.FindStringSubmatch(remoteURLString)
 	if len(matchingResult) == 0 {
-		return repoInfo{"", "", ""}, errors.New("something went wrong")
+		return repoInfo{"", "", ""}, errors.New(invalidURLMessage)
 	}
 	return repoInfo{matchingResult[1], matchingResult[2], matchingResult[3]}, nil
 }
