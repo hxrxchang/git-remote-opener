69087790e274f872d474010051913069b7293d2c
hxrxchang <m.y.y.0326.cg@gmail.com>
2021-02-18 01:55:38 +0900

    run: go fmt

--- cmd/git-remote-opener/main_test.go
+++ cmd/git-remote-opener/main_test.go
@@ -9,11 +9,12 @@ import (
 
 func Test_Main(t *testing.T) {
 	ctrl := gomock.NewController(t)
-    defer ctrl.Finish()
+	defer ctrl.Finish()
 
-    mockCommander := mock_main.NewMockICommander(ctrl)
+	mockCommander := mock_main.NewMockICommander(ctrl)
 	out := []byte("origin  git@github.com:hxrxchang/git-remote-opener.git (fetch)\norigin  git@github.com:hxrxchang/git-remote-opener.git (push)")
-    mockCommander.EXPECT().GetGitRemoteInfo().Return(out, nil)
+	mockCommander.EXPECT().GetGitRemoteInfo().Return(out, nil)
+	mockCommander.EXPECT().Open("https://github.com/hxrxchang/git-remote-opener").Return(nil)
 
 	result := _main(mockCommander)
 
